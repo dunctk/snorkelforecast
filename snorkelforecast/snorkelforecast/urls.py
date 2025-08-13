113d9279e5228cddb10bc948cf5d29a9cb6322e0
Duncan Trevithick <d@uncan.net>
2025-08-13 15:42:41 +0000

    fix: Correct SyntaxError in urls.py for robots.txt pattern

--- snorkelforecast/snorkelforecast/urls.py
+++ snorkelforecast/snorkelforecast/urls.py
@@ -34,6 +34,11 @@ urlpatterns = [
     path("", include("conditions.urls")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
     url(r'^robots.txt
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / "static")
 , serve, {'path': 'robots.txt', 'document_root': settings.STATIC_ROOT}),
 ]
 
